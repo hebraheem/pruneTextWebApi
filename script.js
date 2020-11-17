77388ea7894bea2b94afd26081474860dd5e3ee8
hebraheem <iadebiyi01@gmail.com>
2020-11-17 17:59:31 +0100

    worked on previous button

--- script.js
+++ script.js
@@ -30,6 +30,42 @@ function Next() {
 }
 Next()
 
+////////////////////////////////////////////////////////////////////////////////////////
+
+
+document.querySelector('.prev').addEventListener("click",Previous);
+function Previous() {
+    if (count > 1){
+        count --
+        const page= `page=${count}`
+        //console.log(page)
+        const webapi = fetch(`https://newsapi.org/v2/everything?q=bitcoin&apiKey=bc6dde98f0564ae8885cde0960b424f3&${page}`)
+        .then(res => res.json())
+        .then(data => {
+    
+            if (webapi){
+                document.querySelector('.loading').style.display = "none"
+            }
+            //console.log(data)
+            let datas = data.articles
+            let heading = `<h1 class="jumbotron display-4 text-center">Top News page ${count}</h1>`;
+            let output = ""
+            datas.forEach(data => {
+                output += `<ul class= "list-group m-4" style="width: 100%;">
+                    <li class= "list-group-item"> <strong>Headline:</strong> ${data.title} <br><br> <strong>Story:</strong> ${data.description}</li>
+                </ul>`
+            });
+            document.querySelector('.output').innerHTML = output;
+            document.querySelector('.heading').innerHTML = heading;
+            
+        })
+    } else {
+        count = 1
+    }
+  
+}
+Previous()
+
 
 // function webApi(){
 //     const webapi = fetch('https://newsapi.org/v2/everything?q=bitcoin&apiKey=bc6dde98f0564ae8885cde0960b424f3&page=1')
